e7a0e92d21f48ebb5c4843c22368b4b2da20143d
Marian Serna <marianhalliday@gmail.com>
2018-04-05 15:36:44 -0400

    Displaying album cards

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+import Card from './Card';
+import CardSection from './CardSection';
+
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    headerContentStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
+
+  return (
+    <Card key={title}>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image source={{ uri: thumbnail_image }} style={thumbnailStyle} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image source={{ uri: image }} style={imageStyle} />
+      </CardSection>
+    </Card>
+  );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    // make sure img takes full width:
+    flex: 1,
+    width: null
+  }
+};
+
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
+
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends React.Component {
+  state = {
+    albums: []
+  };
+
   componentDidMount() {
-    console.log('componentDidMount in AlbumList!');
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(res => this.setState({ albums: res.data }));
   }
 
+  renderAlbums = () => {
+    return this.state.albums.map(album => (
+      <AlbumDetail key={album.title} album={album} />
+    ));
+  };
+
   render() {
-    return (
-      <View>
-        <Text>Album List</Text>
-      </View>
-    );
+    // console.log(this.state);
+    return <View>{this.renderAlbums()}</View>;
   }
 }
 
